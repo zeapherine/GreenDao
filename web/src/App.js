e905f94c897e47ce2264cb3f852abed45d5a1629
zeapherine <zeapherine@gmail.com>
2022-04-09 13:44:48 +0530

    Configuration completed for CRA, tailwind, react-router-dom, ----added Home page

---
+++ web/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+
+function App() {
+	return (
+		<div className='App'>
+			<Navbar />
+			<Routes>
+				<Route path='/' element={<Home />} />
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
---
+++ web/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+	return (
+		<div className='mt-10'>
+			<div className='flex flex-col justify-center items-start ml-[355px] pt-28'>
+				<h1 className='text-6xl font-normal'>GreenDAO IS A VENTURE CAPITAL</h1>
+				<h1 className='text-6xl font-normal'>FUND WITH A PROGRESSIVE</h1>
+				<h1 className='text-6xl font-normal'> INVESMENT MODEL</h1>
+
+				<p className='mt-4 text-slate-400'>
+					Based on the DAO structure and it's own management token
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
